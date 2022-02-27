486518a6bd9839bf171198cb85edea7e28f40bad
Ayon <ayonjodder177@gmail.com>
2022-02-27 16:44:15 +0600

    Initial

---
+++ app.js
@@ -0,0 +1,34 @@
+/* fetch("https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com",
+		"x-rapidapi-key": "fdab746e91msh396c32ae260aa5bp147ce3jsn1357eb1a5bad"
+	}
+})
+    .then(res => res.json())
+    .then(data => console.log(data))
+	.catch(err => {
+	console.error(err);
+}); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Auto type animation
+let typed = new Typed(".type-animation-header", {
+	strings: ["Live Update of Covid-19 of all over the World"],
+	typeSpeed: 150,
+	backSpeed: 100,
+	loop: true,
+  });
